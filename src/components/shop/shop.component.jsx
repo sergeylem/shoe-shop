1c773c692ad30704db524ead5629dbd33a9c44fe
Sergey Lem <sergey.lem@gmail.com>
2019-08-02 16:27:57 +0600

    added collection-preview and collection-item

---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import SHOP_DATA from './shop.data';
+import CollectionPreview from '../collection-preview/collection-preview.component';
+
+class ShopPage extends React.Component {
+    state = {
+        collections: SHOP_DATA
+    }
+
+    render() {
+        return (
+            this.state.collections.map(collection => 
+                <CollectionPreview 
+                    id={collection.id} title={collection.title} items={collection.items}
+                />
+
+            )
+        )
+    }
+}
+
+export default ShopPage;
